778125cb4a06107b95f7f4a594cc86e28a411da1
Tebatso <tebatsomangena8@gmail.com>
2025-09-18 04:06:14 +0200

    message routes optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -41,6 +41,17 @@ export const sendMessage = async (req,res) => {
     const { id:receiverId } = req.params;
     const senderId = req.user._id;
 
+    if(!text && !image){
+      return res.status(400).json({ message: 'Message text or image is required' });
+    }
+    if(senderId.equals(receiverId)){
+      return res.status(400).json({ message: 'You cannot send message to yourself' });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if(!receiverExists){
+      return res.status(404).json({ message: 'Receiver not found' });
+    }
+
     let imageUrl;
 
     if(image){
@@ -86,7 +97,7 @@ export const getChatPartners = async (req,res) => {
     const chatPartners = await User.find({ _id: { $in: chatPartnerIds } }).select('-password');
 
     res.status(200).json(chatPartners);
-    
+
   } catch (error) {
     console.error('Error in the getChatPartners', error);
     res.status(500).json({ message: 'Server error' });
